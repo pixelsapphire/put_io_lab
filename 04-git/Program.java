794701a768b0395ccda7f0f5deb21dfa208798a3
RubyNaxela <rubynaxela@gmail.com>
2023-10-25 10:16:40 +0200

    Add javadoc comments to Program.java

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -2,12 +2,21 @@ import java.math.BigDecimal;
 
 public class Program {
 
+    /**
+     * Calculates factorial of given number.
+     * @param n a number
+     * @return n!
+     */
     private static BigDecimal factorial(int n) {
         BigDecimal result = BigDecimal.ONE;
         for (int i = 2; i <= n; i++) result = result.multiply(BigDecimal.valueOf(i));
         return result;
     }
 
+    /**
+     * Application entry point.
+     * @param args command line arguments, one integer expected
+     */
     public static void main(String args[]) {
         System.out.println(factorial(Integer.parseInt(args[0])));
     }
